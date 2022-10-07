d6e8cd53b3c6f82d0e9cd3d1237cddb53cec40d3
Ragnar Dreier <ragnad@stud.ntnu.no>
2022-10-07 16:04:29 +0200

    inital setup

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Navbar.css";
+import SearchBox from "../SearchBox/SearchBox"; 
+// import Logo from "../Logo/Logo";
+import RandomButton from "../RandomButton/RandomButton";
+
+
+const Navbar = () => {
+    return (
+        <div className="Header">
+            <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'></link>
+                <img id="pokeball" src="pokeball.png" alt="Pokeball" width="30" height="30"></img> 
+
+                <h1 id="logo">Pikadex</h1>
+         <div className="searchbox"> 
+                    <SearchBox />
+                    <RandomButton />
+        </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../../components/Navbar/Navbar";
+// import SearchBox from "../../components/SearchBox/SearchBox";
+
+
+
+function MainPage() {
+    
+    return (
+        <Navbar />
+    );
+  }
+  
+export default MainPage;
