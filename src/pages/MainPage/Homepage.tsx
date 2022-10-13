b0f19438583047b184e397a7f9917375983a2806
sindrimt <sindrimt@stud.ntnu.no>
2022-10-13 11:48:01 +0200

    #5 better mobile view

--- src/pages/MainPage/Homepage.tsx
+++ src/pages/MainPage/Homepage.tsx
@@ -13,25 +13,131 @@ function MainPage() {
             <LeftEdge src={leftEdge} />
             <RightEdge src={rightEdge} />
             <FooterEdge src={footerEdge} />
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
         </>
     );
 }
 
 const LeftEdge = styled.img`
-    position: absolute;
-    left: -1px;
-    top: 24%;
+    position: fixed;
+    transform: translateX(-1px);
+    top: 200px;
+    height: 70vh;
+
+    @media (max-height: 680px) {
+        height: 64vh;
+    }
+
+    @media (max-height: 550px) {
+        height: 55vh;
+    }
+
+    @media (max-height: 450px) {
+        height: 45vh;
+    }
+
+    @media (max-height: 400px) {
+        height: 40vh;
+    }
 `;
 
 const RightEdge = styled.img`
-    position: absolute;
+    position: fixed;
     right: 0px;
-    top: 24%;
+    transform: translateX(0px);
+    top: 200px;
+    height: 70%;
+
+    @media (max-height: 680px) {
+        height: 64vh;
+    }
+
+    @media (max-height: 550px) {
+        height: 55vh;
+    }
+
+    @media (max-height: 450px) {
+        height: 45vh;
+    }
+
+    @media (max-height: 400px) {
+        height: 40vh;
+    }
 `;
 
 const FooterEdge = styled.img`
-    position: absolute;
-    bottom: 0px;
+    position: fixed;
+    bottom: -1px;
     left: 50%;
     transform: translate(-50%, 0%);
 `;
