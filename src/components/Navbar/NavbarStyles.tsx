bd8069ea4e4474ba9cb79b32e9ddd696b8e0c7d7
sindrimt <sindrimt@gmail.com>
2022-10-13 10:54:34 +0200

    #5 Added tags (Navbar)
    Co-authored-by: Marcus Stephan Nordal <marcussn@stud.ntnu.no>

--- src/components/Navbar/NavbarStyles.tsx
+++ src/components/Navbar/NavbarStyles.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 // Media queries https://www.w3schools.com/css/css_rwd_mediaqueries.asp
 
+//The wrapper for our Navbar
 export const NavbarOuter = styled.div`
     @media (max-width: 600px) {
         position: absolute;
@@ -11,6 +12,7 @@ export const NavbarOuter = styled.div`
     }
 `;
 
+//Wrapper for the logo and the textbox
 export const LogoContainer = styled.div`
     @media (max-width: 600px) {
         display: flex;
@@ -59,6 +61,7 @@ export const PokeBall = styled.img`
     }
 `;
 
+//The grid creates equally sized rows. Used by the search bar, pokeball and the type-tags.
 export const GridContainer = styled.div`
     @media (max-width: 600px) {
         display: grid;
@@ -69,6 +72,7 @@ export const GridContainer = styled.div`
     }
 `;
 
+//Wrapper for the searchbar and pokeball button
 export const SearchOuter = styled.div`
     @media (max-width: 600px) {
         display: grid;
@@ -103,19 +107,81 @@ export const PokeBallBtn = styled.img`
     }
 `;
 
+//Wrapper for the type-tags
 export const TagOuter = styled.div`
     display: flex;
     height: 30px;
     gap: 20px;
+    overflow-y: scroll;
+
+    -ms-overflow-style: none; /* for Internet Explorer, Edge */
+    scrollbar-width: none; /* for Firefox */
+    overflow-y: scroll;
+
+    ::-webkit-scrollbar {
+        display: none; /* for Chrome, Safari, and Opera */
+    }
 `;
 
+//Type tag such as Water, Bug, Fire, etc.
 export const Tag = styled.div`
-    background-color: #74cb48;
     color: #ffffff;
-    width: 80px;
+    padding: 2px 10px 2px 10px;
     display: flex;
     align-items: center;
     justify-content: space-evenly;
     border-radius: 10px;
-    //flex-grow: 1;
+`;
+//The bottom edge of the navbar
+export const FilterOuter = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+`;
+
+export const NavbarEdge = styled.img`
+    margin-top: 10px;
+
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+//Filter icon to the left of the text
+export const FilterIcon = styled.div``;
+
+//"Filter your pokemon"
+export const FilterText = styled.div``;
+
+//The dropdown appears when "Filter your pokemon" is clicked
+export const DropdownOuter = styled.div``;
+
+//Header for "Types" and "Sort by"
+export const DropdownHeader = styled.div`
+    color: #ffffff;
+`;
+
+//Box and grid for the types-section
+export const TypesBox = styled.div`
+    background-color: rgba(255, 255, 255, 0.2);
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+`;
+
+//Box and grid for the sort-by-section
+export const SortByBox = styled.div`
+    background-color: rgba(255, 255, 255, 0.2);
+    display: grid;
+    grid-template-columns: auto(minmax(100px, 1fr));
+`;
+
+//"Sort by"-tags such as "A - Z"
+export const SortTag = styled.div`
+    background-color: rgba(15, 10, 10, 0.5);
+    color: #ffffff;
+    padding: 2px 10px 2px 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    border-radius: 10px;
 `;
