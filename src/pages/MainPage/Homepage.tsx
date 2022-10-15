90d7642c548bbed94a23032c58d50675c27e45ef
Marcus Stephan Nordal <marcussn@stud.ntnu.no>
2022-10-15 17:29:24 +0200

    Display data from query in card
    Co-authored-by: sindrimt@stud.ntnu.no

--- src/pages/MainPage/Homepage.tsx
+++ src/pages/MainPage/Homepage.tsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+
 import Navbar from "../../components/Navbar/Navbar";
 import Card from "../../components/Card/Card";
 
@@ -9,11 +11,41 @@ import footerEdge from "../../assets/pageEdges/footerEdge.svg";
 import styled from "styled-components";
 import { useScroll } from "../../hooks/useScroll";
 
+import { useQuery, gql, ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "https://beta.pokeapi.co/graphql/v1beta",
+    cache: new InMemoryCache(),
+});
+
+const GET_POKEMON_LIST = gql`
+    {
+        pokemon_v2_pokemonsprites(limit: 10) {
+            pokemon_id
+            pokemon_v2_pokemon {
+                id
+                name
+            }
+            sprites
+        }
+    }
+`;
+
 const MainPage = () => {
     const scroll = useScroll();
+
     let lineHeight: string = "70vh";
     let lineTop: string = "200px";
     let lineOffset: string = "0px";
+
+    const [pokemonData, setPokemonData] = useState<any>([]);
+
+    useEffect(() => {
+        client.query({ query: GET_POKEMON_LIST }).then((response) => {
+            console.log(response);
+            setPokemonData(response);
+        });
+    }, []);
 
     // If the user has scrolled over 80 px, increase the line heights
     if (scroll > 5) {
@@ -28,10 +60,16 @@ const MainPage = () => {
             <LeftEdge src={leftEdge} height={lineHeight} top={lineTop} offset={lineOffset} />
             <RightEdge src={rightEdge} height={lineHeight} top={lineTop} offset={lineOffset} />
             <CardGrid>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                {pokemonData?.data?.pokemon_v2_pokemonsprites?.map((pokemon: any, index: number) => {
+                    return (
+                        <Card
+                            key={index}
+                            img={JSON.parse(pokemon.sprites).front_default}
+                            index={pokemon.pokemon_id}
+                            name={pokemon.pokemon_v2_pokemon.name}
+                        />
+                    );
+                })}
                 <FooterEdge src={footerEdge} />
             </CardGrid>
         </>
